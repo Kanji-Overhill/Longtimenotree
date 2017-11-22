dcbbd90f4acaa0edb5b45d73cd6307bf26d6f276
Kanji-Overhill <ingluisfelipe07@gmail.com>
2017-11-22 16:41:49 -0600

    2 Fix Primera Version

--- tools.php
+++ tools.php
@@ -14,30 +14,30 @@ if(isset($_POST['send'])) {
 		$email_message .= "Last Name: " . $_POST['last_name'] . "\n";
 		$email_message .= "Email: " . $_POST['email'] . "\n";
 		$email_message .= "Phone: " . $_POST['phone'] . "\n";
-		$email_message .= "Shipping Address: " . $_POST['shipping'] . "\n";
+		$email_message .= "Shipping Address: " . $_POST['shipping'] . "\n\n";
 		$email_message .= "Apartment: " . $_POST['apartment'] . "\n";
 		$email_message .= "City: " . $_POST['city'] . "\n";
 		$email_message .= "Zip Code: " . $_POST['zip'] . "\n";
-		$email_message .= "Payment information: " . $_POST['payment'] . "\n";
-		$email_message .= "Notes For Delivery Driver: " . $_POST['notes'] . "\n";
+		$email_message .= "Payment information: " . $_POST['payment'] . "\n\n";
+		$email_message .= "Notes For Delivery Driver: " . $_POST['notes'] . "\n\n";
 		$email_message .= "How did you Hear About Us: " . $_POST['stand'] . "\n\n";
 
-		$email_message2 = "Dear ".$_POST["first_name"].",<br>
+		$email_message2 = "Dear ".$_POST["first_name"].",
 
-		Thank you for your Christmas Tree order!<br><br>
+		Thank you for your Christmas Tree order!
 
 		Deliveries are scheduled for every Tuesday and Friday up until Christmas day. Orders must be made
-		48hrs. in advance in order to be confirmed.<br><br>
+		48hrs. in advance in order to be confirmed.
 
-		We will notify you once your tree is out for delivery.<br>
+		We will notify you once your tree is out for delivery.
 		Sit tight while we get your tree cut and delivered, you will have the smell of a fresh cut tree in your
-		home very soon.<br><br>
+		home very soon.
 
 
-		Thanks again and Happy Holidays!<br><br>
+		Thanks again and Happy Holidays!
 
-		970-471-3312<br>
-		JJ@VailButler.com<br>
+		970-471-3312
+		JJ@VailButler.com
 		www.longtimenotree.com";
 
 		$headers = 'From: '.$email_from."\r\n".'Reply-To: '.$email_from."\r\n".'X-Mailer: PHP/'.phpversion();
